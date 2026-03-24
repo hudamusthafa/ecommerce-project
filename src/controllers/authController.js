a6f59de80cf91ddcfbef64f63066953c8495fc14
Huda Musthafa MK <hudamusthafa111@gmail.com>
2026-03-24 12:57:38 +0530

    Week 1: Completed authentication (register, login, validation, security fixes)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,19 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
+
+//-------------register------------
+
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    const userExists = await User.findOne({ email });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+     const emailLower = email.toLowerCase();
+
+    const userExists = await User.findOne({ email:emailLower});
 
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
@@ -16,15 +24,71 @@ exports.register = async (req, res) => {
 
     const user = await User.create({
       name,
-      email,
+      email: emailLower,
       password: hashedPassword,
     });
 
-    res.json({
-      message: "User registered successfully",
-      user,
-    });
+    return res.json({
+  message: "User registered successfully",
+  user: {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+  },
+});
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
+//------------login----------------------
+
+
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+   
+
+    // check user exists
+     const emailLower = email.toLowerCase();
+
+    const user = await User.findOne({ email:emailLower });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    // check blocked user
+    if (user.isBlocked) {
+      return res.status(403).json({ message: "User is blocked" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // send response (without password)
+    return res.json({
+      message: "Login successful",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
