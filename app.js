4a583bb6b6b8984644dce8a1c277f0277f3662c4
Huda Musthafa MK <hudamusthafa111@gmail.com>
2026-03-24 14:25:15 +0530

    Week 1: Added login page UI

--- app.js
+++ app.js
@@ -9,17 +9,26 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use(express.static(path.join(__dirname, "public")));
 
 // DB connection
 connectDB();
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("API is running...");
+// Middleware
+app.use(express.json());
+
+// Static
+app.use(express.static(path.join(__dirname, "public")));
+
+// API routes
+app.use("/api/auth", authRoutes);
+
+// Page routes
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "register.html"));
 });
 
 app.listen(process.env.PORT, () => {
